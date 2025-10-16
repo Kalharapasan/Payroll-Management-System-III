c31db5e51bd162adf137ad84aacff9945de10fe7
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-10-16 20:39:31 +0530

    Create Js file

--- index.php
+++ index.php
@@ -387,6 +387,9 @@
         </div>
     </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js.js"></script>
+
 
     
 </body>
